f1b0950289993109a446acd7285f6b5863e49adc
gmcln-rct <producersama@gmail.com>
2022-11-25 13:14:42 -0500

    Add faux response

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,5 +1,8 @@
 function handler(req, res) {
-// Code inside here will not be exposed to visitors
+    // Code inside here will not be exposed to visitors
+
+    res.status(200).json({ message: 'This req works!' });
+
 
 }
 
