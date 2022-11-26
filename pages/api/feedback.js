bf5d592b74fa7043e891bd1da664249c367b4b56
gmcln-rct <producersama@gmail.com>
2022-11-26 17:06:22 -0500

    Cleanup

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,9 +1,8 @@
+
+
 function handler(req, res) {
     // Code inside here will not be exposed to visitors
-
     res.status(200).json({ message: 'This req works!' });
-
-
 }
 
 export default handler;
