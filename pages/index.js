2718483fa99479db62c12dbac09206c42370e88a
gmcln-rct <producersama@gmail.com>
2022-11-27 12:15:07 -0500

    add form

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,20 @@ function HomePage() {
   return (
     <div>
       <h1>The Home Page</h1>
+      <form>
+        <div>
+        <label htmlFor="email">Your Email Address</label>
+        <input type="email" id="email" />
+        </div>
+        <div>
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" />
+        </div>
+        <div>
+        <label htmlFor="message">Message</label>
+        <textarea id="message" rows="5"></textarea>
+        </div>
+        </form>
     </div>
   );
 }
