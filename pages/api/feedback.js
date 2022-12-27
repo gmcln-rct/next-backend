63c9d3aa4773b73a4d215e4676aafe1227f63de6
gmcln-rct <producersama@gmail.com>
2022-12-27 17:23:06 -0500

    Work on req check

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,4 +1,17 @@
 function handler(req, res) {
+    if (req.method === 'POST') {
+        const email = req.body.email;
+        const feedbackText = req.body.text;
+
+        const newFeedback = {
+            id: new Date().toISOString(),
+            email: email,
+            text: feedbackText
+        };
+
+        console.log(newFeedback);
+        res.status(201).json({ message: 'Success!', feedback: newFeedback });
+    }
     // Code inside here will not be exposed to visitors
     res.status(200).json({ message: 'This req works!' });
 }
