45c7e5b7ee7b290867ba71b12882e064fa3b60e2
gmcln-rct <producersama@gmail.com>
2022-12-27 18:51:20 -0500

    Store data

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -13,7 +13,13 @@ function handler(req, res) {
             text: feedbackText
         };
 
-        console.log(newFeedback);
+        // Store that in a database or in a file
+        const filePath = path.join(process.cwd(), 'data', 'feedback.json');
+        const fileData = fs.readFileSync(filePath);
+        const data = JSON.parse(fileData);
+        data.push(newFeedback);
+        fs.writeFileSync(filePath, JSON.stringify(data));
+        // console.log(newFeedback);
         res.status(201).json({ message: 'Success!', feedback: newFeedback });
     }
     // Code inside here will not be exposed to visitors
