f4facc418eea705083c76b3eb00b2eed418eddbc
gmcln-rct <producersama@gmail.com>
2023-01-03 10:17:20 -0500

    Work on feedback

--- pages/api/[feedbackId].js
+++ pages/api/[feedbackId].js
@@ -1,13 +1,32 @@
 import { buildFeedbackPath, extractFeedback } from '../api/feedback';
 
 function handler(req, res) {
-  const feedbackId = req.query.feedbackId;
+    // if (req.method === 'POST') {
+    //     const email = req.body.email;
+    //     const text = req.body.text;
+
+    //     const newFeedback = {
+    //         id: new Date().toISOString(),
+    //         email: email,
+    //         text: text
+    //     };
+
+    //     // Store that in a database or in a file
+    //     const filePath = buildFeedbackPath();
+    //     const data = extractFeedback(filePath);
+    //     data.push(newFeedback);
+    //     fs
+
+    //     res.status(201).json({ message: 'Success!', feedback: newFeedback });
+    //     return;
+    // }
+    const feedbackId = req.query.feedbackId;
     const filePath = buildFeedbackPath();
     const feedbackData = extractFeedback(filePath);
     const selectedFeedback = feedbackData.find(item => item.id === feedbackId);
 
     res.status(200).json({ feedback: selectedFeedback });
-  
+
 }
 
 export default handler;
