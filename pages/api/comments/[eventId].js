3a6463f6ceebb963c40df9960210ec73703a312e
gmcln-rct <producersama@gmail.com>
2023-01-18 18:24:25 -0500

    Work on comment api

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,5 +1,9 @@
 function handler(req, res) {
+
+    const { eventId } = req.query.eventId;
+
   if (req.method === 'POST') {
+    // add server-side validation
     const { email, name, text } = req.body;
 
     if (
@@ -10,9 +14,10 @@ function handler(req, res) {
       !text ||
       text.trim() === ''
     ) {
-      res.status(422).json({ message: 'Invalid input. Try harder' });
+      res.status(422).json({ message: 'Invalid input. You are a failure' });
       return;
     }
+    console.log(email, name, text);
 
     const newComment = {
       id: new Date().toISOString(),
@@ -25,4 +30,18 @@ function handler(req, res) {
 
     res.status(201).json({ message: 'Added comment', comment: newComment });
   }
+
+    if (req.method === 'GET') {
+    // const eventId = req.query.eventId;
+        
+    // const client = await MongoClient.connect(MONGODB_URI);
+    // const db = client.db();
+    // const commentsCollection = db.collection('comments');
+    // const selectedComments = await commentsCollection.find({ eventId
+    // }).toArray();
+    // client.close();
+    // res.status(200).json({ comments: selectedComments });
 }
+}
+
+export default handler;
