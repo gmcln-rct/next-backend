99a470aeed7b971d993253646277141641b18bde
gmcln-rct <producersama@gmail.com>
2023-01-19 20:08:32 -0500

    Work on commments api fetch

--- components/input/comments.js
+++ components/input/comments.js
@@ -22,10 +22,14 @@ function Comments(props) {
       headers: {
         'Content-Type': 'application/json',
       },
+    }).then((response) => {
+      if (response.ok) {
+        console.log('Success!');
+      }
+    }).then((data) => {
+      console.log(data);
     });
 
-
-    
   }
 
   return (
