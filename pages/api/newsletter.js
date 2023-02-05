aa603fe66bd95ed323a409478c3cd92ac7cfc954
gmcln-rct <producersama@gmail.com>
2023-02-05 14:15:45 -0500

    Fix db util error

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,20 +1,6 @@
 
 import { connectDatabase, insertDocument } from '../../helpers/db-util';
 
-// async function connectDatabase () {
-//   const client = await MongoClient.connect(
-//     'mongodb+srv://newsletteruser:S6iHou7gpgVhOS2k@cluster2.q5n56ip.mongodb.net/?retryWrites=true&w=majority'
-//   );
-
-//   return client;
-// }
-
-// async function insertDocument(client, document) {
-//   const db = client.db();
-
-//   return db.collection('newsletter').insertOne(document);
-// }
-
 async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
